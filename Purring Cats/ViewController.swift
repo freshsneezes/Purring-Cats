5feaf18b76f72fcadb2f1841749ad4acb3e6d77c
freshsneezes <freshsneezes@users.noreply.github.com>
2017-07-28 11:39:03 +0800

    Initial Commit

---
+++ Purring Cats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Purring Cats
+//
+//  Created by its on 28/7/2017.
+//  Copyright © 2017 freshsneezes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Purring CatsTests/Purring_CatsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Purring_CatsTests.swift
+//  Purring CatsTests
+//
+//  Created by its on 28/7/2017.
+//  Copyright © 2017 freshsneezes. All rights reserved.
+//
+
+import XCTest
+@testable import Purring_Cats
+
+class Purring_CatsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
